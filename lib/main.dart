97568ea9caea8a5edee952f277b3621f1b863236
Amar <amarjaiswar1606@gmail.com>
2023-08-03 13:45:12 +0530

    device orientaion check

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
+import 'package:flutter/services.dart';
 
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
@@ -10,7 +11,12 @@ var kDarkColorScheme =
       seedColor: const Color.fromARGB(255, 5, 99, 125));
 
 void main() {
-  runApp(
+
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]).then((value) =>   
+   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
         useMaterial3: true,
@@ -54,4 +60,6 @@ void main() {
       home: const Expenses(),
     ),
   );
+// );
+
 }
